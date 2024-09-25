1b0028b5c1d8cc26e6c5a7a4fad6ed976b27929f
Mulukuntla <yours.saisaketh@gmail.com>
2024-09-25 17:44:59 +0530

    nodemon debugger

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const http=require('http')
 const routes=require('./routes')
-console.log(routes.someText)
-const server=http.createServer(routes.handler)
+
+const server=http.createServer(routes)
 server.listen(4000)
