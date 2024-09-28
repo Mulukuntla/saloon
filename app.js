30af5e8a7ed6bcddfc428d259747925a034c9e37
Mulukuntla <yours.saisaketh@gmail.com>
2024-09-28 16:48:09 +0530

    express.js...routes

--- app.js
+++ app.js
@@ -2,29 +2,16 @@
 const express= require('express');
 const bodyparser=require('body-parser')
 const app=express();
+const adminRouter=require('./routes/admin')
+const shopRouter=require('./routes/shop')
 app.use(bodyparser.urlencoded({extended:false}))
+app.use('/shop',shopRouter)
+app.use('/admin',adminRouter)
 
-app.use("/add-product",(req,res,next)=>{
-    res.send(`
-        <form action="/product" method="POST">
-            <input type="text" name="title">
-            <input type="number" name="size">
-            <button type="submit">Add Product</button>
-        </form>
-    `);
-    
-    
-       
-})
-app.post("/product",(req,res,next)=>{
-    console.log(req.body)
-    res.redirect('/')
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page Not Found</h1>')
 })
 
-app.use("/",(req,res,next)=>{
-   
-    res.send('<h1>Hello from Express.js</h1>')
-})
 
 
 app.listen(4000);
