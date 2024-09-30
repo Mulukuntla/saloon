f78a3b31310771e5f74eaa663592604eedc9b846
Mulukuntla <yours.saisaketh@gmail.com>
2024-09-30 18:22:45 +0530

    Dynamic routes 1

--- routes/contactUs.js
+++
@@ -1,9 +0,0 @@
-const path=require('path')
-const express=require('express')
-const rootdir=require('../util/path')
-const productsController=require('../controllers/products')
-const router=express.Router()
-
-router.get("/contactUs",productsController.contactUsCon)
-router.post("/success",productsController.successcon)
-module.exports=router;
