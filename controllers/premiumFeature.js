4224a0d8a8bce2f89fea8ed89d6ac8ef03f879ea
Mulukuntla <yours.saisaketh@gmail.com>
2024-11-12 10:29:19 +0530

    Leaderboard Optimisation Backend

--- controllers/premiumFeature.js
+++ controllers/premiumFeature.js
@@ -1,34 +1,31 @@
 
-
 const User= require("../models/Expense")
-const Expense= require("../models/ExpenseTracker")
+const ExpenseTracker= require("../models/ExpenseTracker")
 const jwt=require("jsonwebtoken")
+const sequelize=require("../util/database");
+
 
 const getUserLeaderBoard=async (req,res,next)=>{
     try{
-        const users=await User.findAll()
-        const expenses=await Expense.findAll()
-        const userAggregatedExpenses=[]
-        expenses.forEach(expense => {
-            if(userAggregatedExpenses[expense.userId]){
-                userAggregatedExpenses[expense.userId]=userAggregatedExpenses[expense.userId]+expense.expense
-            }
-            else{
-                userAggregatedExpenses[expense.userId]=expense.expense
-            }
-            
-        })
-        var userLeaderBoardDetails=[];
-        users.forEach((user)=>{
-            userLeaderBoardDetails.push({name:user.userName,total_cost:userAggregatedExpenses[user.id]} || 0)
+        const userLeaderBoardDetails=await User.findAll({
+            attributes:["id","Username",[sequelize.fn("sum",sequelize.col("ExpenseTrackers.expense")),"total_cost"]],
+            include:[
+                {
+                model:ExpenseTracker,
+                attributes:[]
+                }
+            ],
+            group:["User.id"],
+            order:[["total_cost","DESC"]]
+
 
         })
-        userLeaderBoardDetails.sort((a,b)=>b.total_cost-a.total_cost)
-
+        console.log(userLeaderBoardDetails[0])
         res.status(200).json({userLeaderBoardDetails})
 
     }
     catch(err){
+        console.log(err)
         res.status(500).json(err)
 
     }
