6c2d80258dd1e203d85d6162d203c994258cc4cd
Mulukuntla <yours.saisaketh@gmail.com>
2024-11-12 17:16:15 +0530

    Making expense app production ready Backend

--- controllers/ExpenseTracker.js
+++ controllers/ExpenseTracker.js
@@ -1,5 +1,6 @@
 const Expense= require("../models/ExpenseTracker")
 const totalExpensess= require("../models/Expense")
+const sequelize = require("../util/database")
 function isstringvalid(string){
     if(string.length===0 || string==undefined){
         return true
@@ -10,6 +11,7 @@ function isstringvalid(string){
 }
 
 const addExpense= async (req,res,next) =>{
+  const t=await sequelize.transaction()
     try{
       const expense=req.body.expense;
       const description=req.body.description;
@@ -21,16 +23,23 @@ const addExpense= async (req,res,next) =>{
       console.log(expense,description,category)
       
       
-      const data=await Expense.create({expense:expense,description:description,category:category,userId:req.user.id})
+      const data=await Expense.create({expense:expense,description:description,category:category,userId:req.user.id},{transaction:t})
       await res.status(201).json({newUserDetail:data,success:true});
       const totalExpen=Number(req.user.totalExpenses)+Number(expense)
       console.log(totalExpen)
-      await totalExpensess.update(
-          {totalExpenses:totalExpen},
-          {where:{id:req.user.id}}
+      await totalExpensess.update({
+            totalExpenses:totalExpen
+          },
+          {
+            where:{id:req.user.id},
+            transaction:t
+          }
         )
+
+      await t.commit()
       }
       catch(err){
+        await t.rollback()
         console.log(err)
         res.status(500).json(err)
       }
@@ -63,26 +72,39 @@ const getExpense=async (req,res,next)=>{
   }
 
 const deleteExpense= async (req,res) => {
-  
-      
+  const t=await sequelize.transaction()  
+    try{
+    
       
     const uId=req.params.id;
+    const expense=await Expense.findOne({where:{id:req.params.id}})
+    console.log(expense.expense)
+    const user=await totalExpensess.findOne({where:{id:req.user.id}})
+    var expenses=Number(user.totalExpenses)-Number(expense.expense)
+    await user.update({totalExpenses:expenses},{transaction:t})
     if(isstringvalid(uId)){
         return res.status(400).json({success:false,message:"not an valid id"})
     }
-    Expense.destroy({where:{userId:uId,userId:req.user.id}})
+    Expense.destroy({where:{userId:uId,userId:req.user.id}},{transaction:t})
     .then((noofrows)=>{
+      
       if(noofrows===0){
         return res.status(404).json({success:false,message:"Expense doesnot belong to user"})
 
       }
+     
     res.status(200).json({ide:uId,success:true,message:"deleted Successfully"});
+    
 
     })
-    .catch(err =>{
+    await t.commit()
+  }
+    catch(err){
+      await t.rollback()
+        console.log(err)
         res.status(500).json({error:err,success:false,message:"failed"})
 
-    })
+  }
       
    
   }
